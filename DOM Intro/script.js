d5327ca255754642d80f2e96a7c1539dd61ecb35
Aman Diwakar <157607718+amandiwakar86@users.noreply.github.com>
2024-09-27 21:38:06 +0530

    Add files via upload

---
+++ DOM Intro/script.js
@@ -0,0 +1,52 @@
+// JavaScript HTML DOM
+// with the HTML DOM JavaScript can access and change all the elements of an HTML Document
+
+// The HTML DOM (Document Object Model)
+// When a web page is loaded, the browser create a document Object Model of the page.
+// The HTML DOM model is constructed as a tree od objects:
+// With the object model, JavaScript gets all the power it needs to create dynamic HTML:
+// 1. JavaScript can change all the HTML elemants in the page
+// 2. JavaScript can change all the HTML attribute in the page
+// 3. JavaScript can change all the CSS style in the page
+// 4. JavaScript can remove existing HTML elements and attributes
+// 5. JavaScript can add new HTMl attribute and elements
+// 6. JavaScript can react to all existing HTML events in the page
+// 7. JavaScript can create new HTML events i the page
+
+// WHAT YOU WILL LEARN ----------------->
+// 1. How to change the contant of HTML elements
+// 2. How to change the style (CSS) of HTML elements
+// 3. How to react to HTML DOM events
+// 4. How to add and delete HTML elements
+
+
+// -------------> What is the DOM <-------------//
+
+/* 
+The DOM is w3c ( World Wide Web Consortium ) standard
+The DOM defines a standard for accessing documents:
+*/
+
+/*
+The w3c Document Object Model (DOM) is a platform and language-neutral interface that allow prorams and scripts to dynamically access and update the contant, structure, and style of a document.
+*/
+
+// The W3C DOM standard is separated into 3 different parts:
+
+/*
+1. Core DOM - standard model for all document types
+2. XML DOM - srandard model for XML documents
+3. HTML DOM - standard model for HTML documents
+*/
+
+// What is the HTML DOM
+
+/*
+The HTML DOM is a standard object model and programming interface for HTML. It define:
+1. The HTML elements as objects
+2. The properties of all HTML elements
+3. The methods to access all HTML elements
+4. The events for all the HTML elements
+*/
+
+// In other words: The HTML DOM is a standard for how to get, change, add or delete HTML elements.
